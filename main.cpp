843b42b36c06ad78c308dd86b5a4ff2660556a25
abishekmahendran <91681257+abishekmahendran@users.noreply.github.com>
2021-11-15 00:38:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 
 int main() {
-    int n, a=0, b=1,c;
+    int n, a=0, b=1,c=0;
     cin >> n ;
     cout << b <<endl;
 
-        for(int i=0; i<=n; i++){
-            if (c<=n){
+        for(int c=0; c<=n; c++){
                 c=a+b;
                 cout << c <<endl;
                 a=b;
                 b=c;
-            }
             }
 
    return 0;
